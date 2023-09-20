7f110f45ea3886f173a04c5883aed9afd7012f50
Timothy Brandt <timothy.brandt@softwareone.com>
2023-09-20 17:42:05 +0200

    mqtt messages

--- hackathon/build-successful.cs
+++ hackathon/build-successful.cs
@@ -1,43 +1,71 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using MQTTnet;
+using MQTTnet.Client;
+
 using Microsoft.Extensions.Logging;
 
-namespace cereal_killers
+namespace cereal_killers;
+
+public class build_successful
 {
-    public class build_successful
+    private readonly ILogger _logger;
+    private static string _broker_Url = "broker.emqx.io";
+
+    public build_successful(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger<build_successful>();
+    }
 
-        public build_successful(ILoggerFactory loggerFactory)
+    [Function("build_successful")]
+    public HttpResponseData RunSuccessful([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+    }
+
+    [Function("build_failed")]
+    public HttpResponseData RunFailed([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+    }
+
+    public static async Task PublishPipelineStatus()
+    {
+        var mqttFactory = new MqttFactory();
+
+        using (var mqttClient = mqttFactory.CreateMqttClient())
         {
-            _logger = loggerFactory.CreateLogger<build_successful>();
-        }
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(_broker_Url)
+                .Build();
 
-        [Function("build_successful")]
-        public HttpResponseData RunSuccessful([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            var applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic("samples/temperature/living_room")
+                .WithPayload("19.5")
+                .Build();
 
-            response.WriteString("Welcome to Azure Functions!");
+            await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
 
-            return response;
-        }
+            await mqttClient.DisconnectAsync();
 
-        [Function("build_failed")]
-        public HttpResponseData RunFailed([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
+            Console.WriteLine("MQTT application message is published.");
         }
     }
 }
